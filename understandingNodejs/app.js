6bd30ef4531308a634e3c6d72f4000d0d815528b
April Copes <antdevelopment1@gmail.com>
2019-07-10 20:46:15 -0400

    Adds example of prototypal inheritance

--- understandingNodejs/app.js
+++ understandingNodejs/app.js
@@ -1,4 +1,25 @@
 const greeting = require("./greet");
-greet();
+// greet();
 // The error that will occur is by design. When requiring a module from another file, if it is a function that hasd not been invoked
 // this will cause an undefined error because functions that are scoped to the file they were created at
+
+// Now using module.exports in our greet file we can invoke the vairable directly since we exported the function
+
+greeting();
+
+function Person(firstname, lastname) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+}
+
+Person.prototype.greet = function() {
+  console.log("Hello, " + this.firstname + this.lastname);
+};
+let john = new Person("John ", "Doe");
+let jane = new Person("Jane ", "Doe");
+john.greet();
+jane.greet();
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
--- understandingNodejs/greet.js
+++ understandingNodejs/greet.js
@@ -1,5 +1,6 @@
-console.log("Hello");
-
 var greet = function() {
   console.log("Hello");
 };
+
+// In order to make this function available outside of this module for use we can...
+module.exports = greet;
