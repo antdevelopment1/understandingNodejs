17099d5bc1cecfaa8539b553a0fb637516d0d0c1
April Copes <antdevelopment1@gmail.com>
2019-07-16 20:53:20 -0400

    selecting an objects properties value by using the varibles saved string value

--- understandingNodejs/events-eventEmitter/app.js
+++ understandingNodejs/events-eventEmitter/app.js
@@ -1,12 +1,21 @@
 // This is a super cool pattern.
-let arr = [];
+// let arr = [];
 
-for (let i = 0; i < 3; i++) {
-  arr.push(function() {
-    console.log(`Hello. I am ${i}`);
-  });
-}
+// for (let i = 0; i < 3; i++) {
+//   arr.push(function() {
+//     console.log(`Hello. I am ${i}`);
+//   });
+// }
 
-arr.forEach(item => {
-  item();
-});
+// arr.forEach(item => {
+//   item();
+// });
+
+let obj = {
+  greet: "Hello"
+};
+
+console.log(obj.greet);
+console.log(obj["greet"]);
+let prop = "greet";
+console.log(obj[prop]);
